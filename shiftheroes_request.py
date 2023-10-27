945e65e5d6da1cccfc50b83289673921d3880495
Chaher <chaher.croissance.public@gmail.com>
2023-10-27 09:55:39 +0200

    Listage planings et creneaux + reservation

--- shiftheroes_request.py
+++ shiftheroes_request.py
@@ -4,8 +4,17 @@ headers = {
     'Authorization': 'Bearer 376f0a25129fda22f7fc44398757efc7',
 }
 
-# Lister les plannings
+# Récupérer la liste des plannings
+response = requests.get('https://shiftheroes.fr/api/v1/plannings', headers=headers)
+# Affiche la réponse JSON convertie en objet Python
+print(response.json())
 
-# Lister les créneaux
+# Récupérer la liste des crénaux d'un planning
+response = requests.get('https://shiftheroes.fr/api/v1/plannings/LQfrZg/shifts', headers=headers)
+# Affiche la réponse JSON convertie en objet Python
+print(response.json())
 
-response = requests.post('https://shiftheroes.fr/api/v1/plannings/LQfrZg/shifts/3kFq5dY/reservations', headers=headers)
+# Récupérer une réservation sur un crénau
+response = requests.post('https://shiftheroes.fr/api/v1/plannings/LQfrZg/shifts/OAF6DPq/reservations', headers=headers)
+# Affiche la réponse JSON convertie en objet Python
+print(response.json())
--- shiftheroes_request.swift
+++ shiftheroes_request.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-let url = URL(string: "https://shiftheroes.fr/api/v1/plannings/LQfrZg/shifts/3kFq5dY/reservations")!
+let url = URL(string: "https://shiftheroes.fr/api/v1/plannings/LQfrZg/shifts/OAF6DPq/reservations")!
 let headers = [
     "Authorization": "Bearer 376f0a25129fda22f7fc44398757efc7"
 ]
